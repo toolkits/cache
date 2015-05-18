8b923124589e7ad3a0d39590ba845c9e255ae17f
Ulric Qin <ulric.qin@gmail.com>
2015-05-18 09:44:34 +0800

    init

---
+++ init.go
@@ -0,0 +1,9 @@
+package cache
+
+import (
+	"time"
+)
+
+func InitCache(host string, idle, max int, toc, tor, tow, defaultExpiration time.Duration) {
+	Instance = NewRedisCache(host, idle, max, toc, tor, tow, defaultExpiration)
+}
